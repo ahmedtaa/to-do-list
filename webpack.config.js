fc98a170aaea8d70034bfb11b5fda813a1e99c10
ahmed tawfik ali <kalkolty@hotmail.com>
2022-01-17 13:28:45 +0200

    fix path in webpack to make it work in gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, ''),
     clean: true,
   },
   module: {
