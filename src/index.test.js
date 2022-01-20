6aabff8753d9ce1949c480411427a2d621ade4eb
zieeco <zieecosam@gmail.com>
2022-01-20 14:31:18 +0100

    Add the test files and import as a module

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+// describe for delete;
+// describe for create;
+
+const { deleteTask, createTask } = require('./index.js');
+
+describe('deleteTask', () => {
+  test('should ', () => {
+    expect(deleteTask('hello')).toBe(); // TODO
+  });
+});
+
+describe('createTask', () => {
+  test('should', () => {
+    expect(createTask()).toBe();// TODO
+  });
+});
